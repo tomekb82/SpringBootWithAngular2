ec1b5fa5c82219dc332d7197e00acaf6fbe7e2bc
Tomasz Belina <tomasz.belina@qualent.eu>
2017-07-13 00:38:28 +0200

    refaktoryzacja na komponenty

--- photoApp/src/app/app.module.ts
+++ photoApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
