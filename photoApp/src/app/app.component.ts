385a9219475ac215c33bb1c4a36610ed011e7d81
Tomasz Belina <tomasz.belina@qualent.eu>
2017-07-13 00:31:42 +0200

    petle

--- photoApp/src/app/app.component.ts
+++ photoApp/src/app/app.component.ts
@@ -6,40 +6,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Przeglądarka zdjęć';
   
-  color = '#ff0000';
+  color = 'white';
+  selected = null;
+
+  edited = {
+    
+  }
+  
+  mode="none";
   
   conf = {
-   adult: '#ff0000',
-   child: '#00ff00',
-   nature: '#0000ff'
+   adult: '#F0E68C',
+   child: '#ADD8E6',
+   nature: '#90EE90'
   }
   
   label = "Wybierz";
   typeSelected = false;
   
-  album = {
+  albums = [{
     name: 'Moje zdjęcia',
     year: 2015,
-    type: 'rodzina',
+    type: '',
+    favourite: false,
+  },
+  { name: 'Fotki z gór',
+    year: 2012,
+    type: '',
     favourite: true,
-  }
+  }];
   
   toggleSelected(){
      this.typeSelected =! this.typeSelected;
   }
   
-  setLabel(label){
-    this.label = label;
-    if(this.label === 'adult'){
-       this.color = this.conf.adult;
-    } else if(this.label === 'child'){
-       this.color = this.conf.child;
-    } else if(this.label === 'nature'){
-       this.color = this.conf.nature;
+  save(event){
+  
+  }
+  
+  getColor(label){
+    if(label === 'Dorosły'){
+       return this.conf.adult;
+    } else if(label === 'Dziecko'){
+       return this.conf.child;
+    } else if(label === 'Natura'){
+       return this.conf.nature;
     }
+  }
+  
+  setType(label){
+    this.label = label;
+    this.selected.type = label;
+    this.selected.color = this.getColor(label)   
     this.toggleSelected();
+  }
+  
+  select(album){
+    if(album !== this.selected){
+        this.mode = "selected";
+        this.selected = album;
+    }
+  }
+
+  edit(album){
+    this.mode = "edit";
+    this.edited = album;
+    this.selected = album;
+  }
+
+  createNew(){
+    this.mode = "edit";
+    var newPlaylist = {};
+    this.selected = newPlaylist;
+    this.edited = newPlaylist;
   }
   
 
--- photoApp/src/app/app.module.ts
+++ photoApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,7 +12,9 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
